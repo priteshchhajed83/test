cfc03da08f618e0dc185e3040bc4a69804c05958
Pritesh <pritesh.s.chhajed@gmail.com>
2022-02-10 11:39:16 +0530

    First Commit

---
+++ src/test/java/com/inetbanking/testCases/sample.java
@@ -0,0 +1,14 @@
+package com.inetbanking.testCases;
+
+import junit.framework.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class sample {
+
+
+    WebDriver driver = new ChromeDriver();
+    
+
+}
